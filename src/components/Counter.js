aecd9c81964f4c64ecd064e1dda4b0d351c111a3
Grzegorz Kaczor <grzesiek125@op.pl>
2021-12-08 18:45:46 +0100

    actions

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,37 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
 const Counter = () => {
-  const toggleHandler = () => {};
+
+  const counter = useSelector(state => state.counter);
+  const dispatch = useDispatch();
+  const show = useSelector(state => state.showCounter)
+
+  const addHandler = () => {
+    dispatch({type: 'increment'});
+  };
+
+  const incHandler = () => {
+    dispatch({type: 'increase', amount: 5});
+  };
+
+  const removeHandler = () => {
+    dispatch({type: 'decrement'});
+  }
+
+  const toggleHandler = () => {
+    dispatch({type: 'toggle'})
+  };
 
   return (
     <main className='counter'>
       <h1>order</h1>
-      <div className='value'>----</div>
+      {show ? <div className='value'>{counter}</div>: null}
+      <div>
+        <button onClick={addHandler}>Add</button>
+        <button onClick={incHandler}>Add by 5</button>
+        <button onClick={removeHandler}>Remove</button>
+      </div>
       <button onClick={toggleHandler}>Toggle</button>
     </main>
   );
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
 
 import './scss/main.scss';
 import App from './App';
+import store from './store/index';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
